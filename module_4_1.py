14baf0c1cd594267e437f0299be7391338705636
DmitriiDrobin <dmitrydrobin0626@gmail.com>
2024-11-02 20:48:05 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,15 @@
+import fake_math as f1
+import true_math as t2
+
+result1 = f1.divide(69, 3)
+
+result2 = f1.divide(3, 0)
+
+result3 = t2.divide(49, 7)
+
+result4 = t2.divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
